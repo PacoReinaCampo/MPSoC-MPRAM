39eedc8274b5ec8627360395bd76371a89487068
PacoReinaCampo <frareicam@gmail.com>
2020-05-31 18:51:03 +0200

    new commit

--- bench/verilog/regression/wb/bfm/mpsoc_msi_wb_bfm_memory.sv
+++ bench/verilog/regression/wb/bfm/mpsoc_msi_wb_bfm_memory.sv
@@ -75,7 +75,7 @@ module mpsoc_msi_wb_bfm_memory #(
   // Constants
   //
 
-  `include "mpsoc_mpram_wb_pkg.v"
+  `include "mpsoc_mpram_wb_pkg.sv"
 
   localparam bytes_per_dw = (DW/8);
   localparam mem_words = (MEM_SIZE_BYTES/bytes_per_dw);
