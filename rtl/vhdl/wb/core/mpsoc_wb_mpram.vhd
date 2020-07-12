e97d6adf5b5c8419e970e40013a6f8d4d3ebad31
PacoReinaCampo <frareicam@gmail.com>
2020-07-12 18:40:10 +0200

    new commit

--- rtl/vhdl/wb/core/mpsoc_wb_mpram.vhd
+++ rtl/vhdl/wb/core/mpsoc_wb_mpram.vhd
@@ -40,7 +40,8 @@
 -- *
 -- * =============================================================================
 -- * Author(s):
--- *   Francisco Javier Reina Campo <frareicam@gmail.com>
+-- *   Olof Kindgren <olof.kindgren@gmail.com>
+-- *   Paco Reina Campo <pacoreinacampo@queenfield.tech>
 -- */
 
 library ieee;
