f660f48fed3b7e7adcc2a5c5b0df4654be99487d
PacoReinaCampo <frareicam@gmail.com>
2022-08-14 19:49:35 +0200

    Working in Peripheral-MPRAM 🔑

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 git config --global user.email "frareicam@gmail.com"
 git config --global user.name "PacoReinaCampo"
 git add *
-git commit -m "Working in Peripheral-MPRAM"
+git commit -m "Working in Peripheral-MPRAM 🔑"
 git push origin master
